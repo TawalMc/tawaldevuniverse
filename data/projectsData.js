005ab1c5ad4833d02a4ecb147107db931d6e12cc
TawalMc <developtawaliou@gmail.com>
2021-06-20 13:50:54 +0100

    Setup all infos about website and change images

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,7 +2,7 @@ const projectsData = [
   {
     title: 'Mes projets',
     description: `Sur ce lien, je fais une liste des projets que j'ai réalisé avec leur description, le lien vers le code source et les démos.`,
-    imgSrc: '/static/images/google.png',
+    imgSrc: '/static/images/github.png',
     href: 'https://github.com/TawalMc/portfolio',
   },
 ]
