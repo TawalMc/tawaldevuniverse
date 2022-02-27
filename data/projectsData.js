c1d96a53bf40a2da1d44bbfc5d999c2785c64771
TawalMc <developtawaliou@gmail.com>
2022-02-27 12:28:20 +0100

    add projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,8 +1,26 @@
 const projectsData = [
   {
-    title: 'My projects',
-    description: `Here is a link to projects that I built. You can read more about them, try a demo and view the source code.`,
+    title: 'Benin Travel (TypeScript - Next.js - Contentful )',
+    description: `Want to explore Benin? Discover our culture, our food, our languages or simply connect with the history of our cities. This platform has been designed for a digital exploration of Benin.`,
+    imgSrc: '/static/images/abomey.png',
+    href: 'https://benin-travel.vercel.app',
+  },
+  {
+    title: 'Real Time Guessing Game (TypeScript - React.js - Socket.io)',
+    description: `Real time guessing game is a multiplayers (02 players) web game where each users try to find the odds emojis when they turn the cards. Each player can only turn two cards each time. If the player finds two cards with same emojis so he wins 1 point. It is an open source projects that you can run on your machine or try on website.`,
     imgSrc: '/static/images/github.png',
+    href: 'https://github.com/TawalMc/GuessingGame',
+  },
+  {
+    title: 'TinyDatabase - TinyDatabase_Arduino (C++ - Arduino)',
+    description: `TinyDatabase is a library that allow the management of persistent internal memory (EEPROM) of microncontroller. With a syntax near to the SQL queries, it encapsulates the writing/reading of EEPROM data which was done with their address by providing a more friendly and simply API.`,
+    imgSrc: '/static/TinyDatabase-logo.png',
+    href: 'https://tinydatabase-doc.vercel.app/',
+  },
+  {
+    title: 'Other projects',
+    description: `Here is a link to the list of my github projects that I built.`,
+    imgSrc: '/static/images/kenpachi.jpeg',
     href: 'https://github.com/TawalMc/portfolio',
   },
 ]
